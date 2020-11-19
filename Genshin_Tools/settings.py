25e25a63920b9649f9dc90b1f7edf41f00733186
Andres Rodriguez <andresmichelrodriguez@gmail.com>
2020-11-19 10:55:11 -0800

    created pity counter page

--- Genshin_Tools/settings.py
+++ Genshin_Tools/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'website.apps.WebsiteConfig',
+    'pity_counter.apps.PityCounterConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- website/urls.py
+++ website/urls.py
@@ -1,6 +1,8 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
+from pity_counter import views as pity_views
 
 urlpatterns = [
     path('', views.home, name='website-home'),
+    path('pity_counter/', pity_views.home, name='pity_counter'),
 ]
