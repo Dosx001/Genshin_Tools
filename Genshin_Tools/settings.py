d8eedcc4ddebc9e7ac642f98be207676bdd7eedd
Andres Rodriguez <andresmichelrodriguez@gmail.com>
2020-11-20 11:59:43 -0800

    Created users app

--- Genshin_Tools/settings.py
+++ Genshin_Tools/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'website.apps.WebsiteConfig',
     'pity_counter.apps.PityCounterConfig',
+    'users.apps.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
