dbbe921119a58d0b5ce08138bab317dba5309417
Andres Rodriguez <andresmichelrodriguez@gmail.com>
2020-11-21 14:16:24 -0800

    Created urls for new pages

--- Genshin_Tools/urls.py
+++ Genshin_Tools/urls.py
@@ -15,8 +15,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from users import views as users_views
-from pity_counter import views as pity_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
