cccaffe7d8a3b703eb54f7258d0c0a0211415bc6
Andres Rodriguez <andresmichelrodriguez@gmail.com>
2020-11-21 13:57:18 -0800

    Update script

--- scripts/openFiles.sh
+++ scripts/openFiles.sh
@@ -10,5 +10,9 @@ elif [ $1 == 'h' ]
 then
     vim -p website/templates/website/base.html \
         website/templates/website/home.html \
+        website/templates/website/post_detail.html \
+        website/templates/website/post_form.html \
+        website/templates/website/user_posts.html \
+        website/templates/website/post_confirm_delete.html \
         pity_counter/templates/pity_counter/pity_counter.html
 fi
