88398824be0b33279131d7eb76a78d11c49a0adf
Andres Rodriguez <andresmichelrodriguez@gmail.com>
2020-11-24 18:25:03 -0800

    Created view

--- pity_counter/views.py
+++ pity_counter/views.py
@@ -1,4 +1,26 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.views.generic import View
+from django.http import JsonResponse
 
-def home(request):
-    return render(request, 'pity_counter/pity_counter.html')
+class PityCounterView(View):
+    def get(self, request):
+        context = {
+            'profile': request.user.profile,
+        }
+        return render(request, 'pity_counter/pity_counter.html', context)
+
+    def post(self, request):
+        text = request.POST.get('buttum_text')
+        print(text)
+        print('test')
+        return JsonResponse({'seconds': 123})
+
+def Weapon10(request):
+    char = request.GET.get('buttum_num', None)
+    if char <= 80:
+        request.user.profile.character = 0
+    else:
+        request.user.profile.character += 10
+    print(request.user.profile.character)
+#    return home(request)
