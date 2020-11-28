bbf78b38290d57f7f5ac35a64ae013782878304b
Andres Rodriguez <andresmichelrodriguez@gmail.com>
2020-11-27 19:49:37 -0800

    testing

--- pity_counter/views.py
+++ pity_counter/views.py
@@ -10,14 +10,28 @@ class PityCounterView(View):
         }
         return render(request, 'pity_counter/pity_counter.html', context)
 
+    def form_valid(self, form):
+        if self.request.is_ajax():
+            form.instance.by = self.request.user
+            print(form.instance.by)
+        else:
+            print('fasd')
+
     def post(self, request):
-        text = request.POST.get('buttum_text')
-        print(text)
+        text = request.POST.get('user')
+        print('testing',text)
         print('test')
+        request.user.profile.character = 10
+        request.user.profile.save()
+        context = {
+            'profile': request.user.profile,
+        }
+        return render(request, 'pity_counter/pity_counter.html', context)
         return JsonResponse({'seconds': 123})
 
 def Weapon10(request):
     char = request.GET.get('buttum_num', None)
+    print(char)
     if char <= 80:
         request.user.profile.character = 0
     else:
