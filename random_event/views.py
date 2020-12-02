6de930e8414b6360bf215941fb69a1dd34618db2
Andres Rodriguez <andresmichelrodriguez@gmail.com>
2020-12-01 20:28:01 -0800

    Updated Random Event page

--- random_event/views.py
+++ random_event/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.views.generic import View
 
+from users.models import Profile
+
 class RandomEventView(View):
     def get(self, request):
         context = {
+            'profile': Profile() if request.user.is_anonymous else request.user.profile
         }
         return render(request, 'random_event.html', context)
