22e35be6a7fdb667c1ee933fd14836f2ec94df34
Andres Rodriguez <andresmichelrodriguez@gmail.com>
2020-12-03 20:17:05 -0800

    Updated Quest Log page

--- quest_log/views.py
+++ quest_log/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from django.views.generic import View
+import json
 
 class QuestLogView(View):
     def get(self, request):
+        with open('quest_log/missions.json') as f:
+            data = json.load(f)
         context = {
+            'Archon': data['Archon'],
+            'World': data['World'],
+            'Commission': data['Commission']
         }
         return render(request, 'quest_log.html', context)
