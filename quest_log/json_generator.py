903fa039526cf84e074de834d29867c6794f3022
Andres Rodriguez <andresmichelrodriguez@gmail.com>
2020-12-11 22:19:32 -0800

    Updated json_generator.py to include Archon quests

--- quest_log/json_generator.py
+++ quest_log/json_generator.py
@@ -52,5 +52,41 @@ data['Commission'].update(new_data)
 data['Commission'] = {i:data['Commission'][i] for i in
         sorted(data['Commission'])}
 
+quests = []
+for line in open('pa1.txt'):
+    line = line[0:len(line)-1]
+    quests.append(line)
+data['Archon']['Prologue']['The Outlander Who Caught the Wind'].update({'quests':quests})
+
+quests = []
+for line in open('pa2.txt'):
+    line = line[0:len(line)-1]
+    quests.append(line)
+data['Archon']['Prologue']['For a Tomorrow Without Tears'].update({'quests':quests})
+
+quests = []
+for line in open('pa3.txt'):
+    line = line[0:len(line)-1]
+    quests.append(line)
+data['Archon']['Prologue']['Song of the Dragon and Freedom'].update({'quests':quests})
+
+quests = []
+for line in open('c1a1.txt'):
+    line = line[0:len(line)-1]
+    quests.append(line)
+data['Archon']['Chapter 1']['Of the Land Amidst Monoliths'].update({'quests':quests})
+
+quests = []
+for line in open('c1a2.txt'):
+    line = line[0:len(line)-1]
+    quests.append(line)
+data['Archon']['Chapter 1']['Farewell, Archaic Lord'].update({'quests':quests})
+
+quests = []
+for line in open('c1a3.txt'):
+    line = line[0:len(line)-1]
+    quests.append(line)
+data['Archon']['Chapter 1']['A New Star Approaches'].update({'quests':quests})
+
 with open('new_missions.json', 'w') as f:
     json.dump(data, f, indent=4)
