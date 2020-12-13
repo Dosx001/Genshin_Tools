a4de16ae66211c942a51adfe72cd70e45f076189
Andres Rodriguez <andresmichelrodriguez@gmail.com>
2020-12-12 16:37:43 -0800

    Cleaned up code

--- quest_log/json_generator.py
+++ quest_log/json_generator.py
@@ -3,41 +3,23 @@ import json
 with open('missions.json') as f:
     data = json.load(f)
 
-new_data = {}
-
-for line in open('Mondstadt.txt'):
-    line = line[0:len(line)-1]
-    if not line in data['World']['Mondstadt']:
-        req = {
-            'Location': None,
-            'Adventure Rank': None,
-            'Archon': None,
-            'World': None,
-            'Commission': None,
-            'NPC': None
-            }
-        new_data.update({line: req})
-data['World']['Mondstadt'].update(new_data)
-data['World']['Mondstadt'] = {i:data['World']['Mondstadt'][i] for i in
-        sorted(data['World']['Mondstadt'])}
-
-new_data = {}
-
-for line in open('Liyue.txt'):
-    line = line[0:len(line)-1]
-    if not line in data['World']['Liyue']:
-        req = {
-            'Location': None,
-            'Adventure Rank': None,
-            'Archon': None,
-            'World': None,
-            'Commission': None,
-            'NPC': None
-            }
-        new_data.update({line: req})
-data['World']['Liyue'].update(new_data)
-data['World']['Liyue'] = {i:data['World']['Liyue'][i] for i in
-        sorted(data['World']['Liyue'])}
+for region in 'Mondstadt', 'Liyue':
+    new_data = {}
+    for line in open(f'{region}.txt'):
+        line = line[0:len(line)-1]
+        if not line in data['World'][f'{region}']:
+            req = {
+                'Location': None,
+                'Adventure Rank': None,
+                'Archon': None,
+                'World': None,
+                'Commission': None,
+                'NPC': None
+                }
+            new_data.update({line: req})
+    data['World'][f'{region}'].update(new_data)
+    data['World'][f'{region}'] = {i:data['World'][f'{region}'][i] for i in
+            sorted(data['World'][f'{region}'])}
 
 new_data = {}
 
@@ -52,41 +34,16 @@ data['Commission'].update(new_data)
 data['Commission'] = {i:data['Commission'][i] for i in
         sorted(data['Commission'])}
 
-quests = []
-for line in open('pa1.txt'):
-    line = line[0:len(line)-1]
-    quests.append(line)
-data['Archon']['Prologue']['The Outlander Who Caught the Wind'].update({'quests':quests})
+files = ['pa1.txt', 'pa2.txt', 'pa3.txt', 'c1a1.txt', 'c1a2.txt', 'c1a3.txt']
+chapters = data['Archon']['Prologue']
+chapters.update(data['Archon']['Chapter 1'])
 
-quests = []
-for line in open('pa2.txt'):
-    line = line[0:len(line)-1]
-    quests.append(line)
-data['Archon']['Prologue']['For a Tomorrow Without Tears'].update({'quests':quests})
-
-quests = []
-for line in open('pa3.txt'):
-    line = line[0:len(line)-1]
-    quests.append(line)
-data['Archon']['Prologue']['Song of the Dragon and Freedom'].update({'quests':quests})
-
-quests = []
-for line in open('c1a1.txt'):
-    line = line[0:len(line)-1]
-    quests.append(line)
-data['Archon']['Chapter 1']['Of the Land Amidst Monoliths'].update({'quests':quests})
-
-quests = []
-for line in open('c1a2.txt'):
-    line = line[0:len(line)-1]
-    quests.append(line)
-data['Archon']['Chapter 1']['Farewell, Archaic Lord'].update({'quests':quests})
-
-quests = []
-for line in open('c1a3.txt'):
-    line = line[0:len(line)-1]
-    quests.append(line)
-data['Archon']['Chapter 1']['A New Star Approaches'].update({'quests':quests})
+for i, chapter in enumerate(chapters):
+    quests = []
+    for line in open(files[i]):
+        line = line[0:len(line)-1]
+        quests.append(line)
+    data['Archon']['Prologue' if i < 3 else 'Chapter 1'][chapter].update({'quests':quests})
 
 with open('new_missions.json', 'w') as f:
     json.dump(data, f, indent=4)
