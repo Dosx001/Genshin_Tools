e82c6a72aff9060ef57e625e61d7b45a65b7a6b8
Andres Rodriguez <andresmichelrodriguez@gmail.com>
2020-12-22 21:35:00 -0800

    Added post request and responese to Resource Converter page

--- resource_converter/views.py
+++ resource_converter/views.py
@@ -1,6 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import View
+import json
 
 class ResourceConverterView(View):
     def get(self, request):
         return render(request, 'resource_converter.html')
+
+    def post(self, request):
+        data = json.loads(request.POST.get('data', None))
+        print(data)
+        return redirect('resource_converter')
--- website/static/resource_converter/main.js
+++ website/static/resource_converter/main.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
             $('#5').remove();
             $('#extra').remove();
         } else if ($('#5')[0] == null) {
-            $('.input-group').append('<div class="input-group-prepend" id="extra">' +
+            $('#Materials').append('<div class="input-group-prepend" id="extra">' +
                 '<span class="input-group-text">5&nbsp;' +
                 '<img src="/media/resource_converter/star.png"></span></div>' +
                 '<input type="number" class="form-control" id="5" value="0">');
@@ -15,7 +15,25 @@ $(document).ready(function() {
     });
 
     $('button').click(function() {
-        var test = $('#ControlSelect')[0];
-        console.log(test.value);
+        var selection = $('#ControlSelect')[0].value;
+        request_data = {
+            activity: selection,
+            materials: {
+                star2: $('input[id=2]')[0].value,
+                star3: $('input[id=3]')[0].value,
+                star4: $('input[id=4]')[0].value
+            }
+        }
+        if (selection != 'Domain of Mastery') {
+            request_data.materials.star5 = $('input[id=5]')[0].value;
+        }
+        $.ajax({
+            url: '',
+            type: 'post',
+            data: {
+                csrfmiddlewaretoken: csrfToken,
+                data: JSON.stringify(request_data)
+            }
+        })
     });
 });
